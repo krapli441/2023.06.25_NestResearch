0afe907b6e544aebd0a7334b325f57e3766eb75c
krapli441 <tlfqj1208@gmail.com>
2023-06-25 15:28:25 +0900

    users 리소스에 대한 crud api 만들기 도전

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Request } from 'express';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('he*lo')
+  getHello(@Req() req: Request): string {
+    console.log(req);
     return this.appService.getHello();
   }
 }
