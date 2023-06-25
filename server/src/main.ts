18d8c7dfed5b7d1536510da6b13425266524f696
krapli441 <tlfqj1208@gmail.com>
2023-06-25 10:51:07 +0900

    Nest에서 CRA로 만들어진 번들 파일을 기준으로 서버 구동 성공

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import path, { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public/build')); // 클라이언트 파일을 제공하기 위해 정적 파일 경로를 생성함
+  await app.listen(3000); // 서버를 3000번 포트에서 실행함.
 }
 bootstrap();
